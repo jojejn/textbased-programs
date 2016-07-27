d3f95986c545c01be0d39b531db3c98c6551c656
jojejn <johnawny1000@gmail.com>
2016-07-27 13:21:49 +0200

    Add files via upload

---
+++ calculator v2.py
@@ -0,0 +1,35 @@
+print("welcome to a calculator")
+i = 0
+
+def add():
+    result = str(num1+num2)
+    print("the result is: " + result)
+def sub():
+    result =str(num1-num2)
+    print("the answer is: "+result)
+def multi():
+    result = str(num1*num2)
+    print("the answer is: "+result)
+def divide():
+    result = str(num1/num2)
+    print("the answer is: "+result)
+for x in range(0,100):
+    print("enter the operation")
+    oper = input()
+    print("first number:")
+    num1 = float(input())
+    print("second number:")
+    num2 = float(input())
+                 
+    
+    if oper == "+":
+       add()
+
+    if oper == "-":
+     sub()    
+
+    if oper == "*":
+        multi()
+
+    if oper == "/":
+        divide()
